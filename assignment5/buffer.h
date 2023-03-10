3557d2b35f6d05fdefc1f342e4fe45f088d3320e
walterbb <44916811+walterbb@users.noreply.github.com>
2023-03-09 22:31:39 -0800

    .

--- assignment5/buffer.h
+++ assignment5/buffer.h
@@ -19,7 +19,7 @@ struct buffer {
 
 // function for initializing array of buffers
 struct buffer **init_buffers() {
-    buffers = (struct buffer **) malloc(sizeof(struct buffer *) * NUM_BUFFERS);
+    struct buffer **buffers = (struct buffer **) malloc(sizeof(struct buffer *) * NUM_BUFFERS);
 
     int i;
     for(i = 0; i < NUM_BUFFERS; i++) {
