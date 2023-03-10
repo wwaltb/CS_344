82524161b62769727534a029fc0cfae5fe13bf5d
walterbb <44916811+walterbb@users.noreply.github.com>
2023-03-09 21:39:36 -0800

    assignment5 initial

---
+++ assignment5/buffer.h
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+#define MAX_LINES 50
+#define LINE_SIZE 1000
+#define NUM_BUFFERS 3
+
+// struct to hold buffer and associated variables
+struct buffer {
+    char buffer[MAX_LINES][LINE_SIZE];
+    int prod_idx;
+    int con_idx;
+    pthread_mutex_t mutex;
+    pthread_cond_t full;
+};
+
+// function for initializing array of buffers
+void init_buffers(struct buffer **buffers) {
+    buffers = malloc(sizeof(struct buffer *) * NUM_BUFFERS);
+
+    int i;
+    for(i = 0; i < NUM_BUFFERS; i++) {
+        buffers[i] = (struct buffer *) malloc(sizeof(struct buffer));
+        buffers[i]->prod_idx = 0;
+        buffers[i]->con_idx = 0;
+        buffers[i]->mutex = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
+        buffers[i]->full = (pthread_cond_t) PTHREAD_COND_INITIALIZER;
+    }
+}
+
+// function for freeing array of buffers
+void free_buffers(struct buffer **buffers) {
+    int i;
+    for(i = 0; i < NUM_BUFFERS; i++) {
+        free(buffers[i]);
+    }
+    free(buffers);
+}
+
+// function to put a line into the buffer
+void put_buffer_line(struct buffer *buffer, char *line) {
+    // lock mutex before putting item into buffer
+    pthread_mutex_lock(&buffer->mutex);
+
+    // put item into buffer
+    strcpy(buffer->buffer[buffer->prod_idx], line);
+
+    // increment producer index
+    buffer->prod_idx++;
+
+    // signal to consumer that buffer isn't empty
+    pthread_cond_signal(&buffer->full);
+
+    // unlock mutex
+    pthread_mutex_unlock(&buffer->mutex);
+}
+
+// function to get a line from the buffer
+void get_buffer_line(struct buffer *buffer, char *line) {
+    // store line from buffer at consumer index and its length
+    strcpy(line, buffer->buffer[buffer->con_idx]);
+    int len = strlen(line);
+
+    // lock mutex before checking if line has data
+    pthread_mutex_lock(&buffer->mutex);
+
+    // wait if line is empty
+    while(len == 0) {
+        pthread_cond_wait(&buffer->full, &buffer->mutex);
+        strcpy(line, buffer->buffer[buffer->con_idx]);
+        len = strlen(line);
+    }
+
+    // increment consumer index
+    buffer->con_idx++;
+
+    // unlock mutex
+    pthread_mutex_unlock(&buffer->mutex);
+}
---
+++ assignment5/line_processor.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "buffer.h"
+
+// create buffers
+struct buffer **buffers;
+
+void *input_thread(void *args) {
+    char input[LINE_SIZE];
+
+    int stop = 0;
+    while(!stop) {
+        fgets(input, LINE_SIZE, stdin);
+
+        if(strcmp(input, "STOP\n")) {
+            stop = 1;
+        }
+
+        put_buffer_line(buffers[0], input);
+    }
+
+    return NULL;
+}
+
+void *output_thread(void *args) {
+    char line[LINE_SIZE];
+
+    int stop = 0;
+    while(!stop) {
+        get_buffer_line(buffers[0], line);
+        printf("line: %s\n", line);
+    }
+
+    return NULL;
+}
+
+
+int main(int argc, char **argv) {
+    // initialize global buffer array
+    init_buffers(buffers);
+
+    // create threads
+    pthread_t input_t, output_t;
+    pthread_create(&input_t, NULL, input_thread, NULL);
+    pthread_create(&output_t, NULL, output_thread, NULL);
+
+    // wait for threads to terminate
+    pthread_join(&input_t, NULL);
+    pthread_join(&output_t, NULL);
+
+    // free global buffer array
+    free(buffers);
+
+    return(0);
+}
